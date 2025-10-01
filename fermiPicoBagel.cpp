999ee581a509c3eeffa11cb65a503db7b121ab22
Ken Howry <131243674+kenhowry@users.noreply.github.com>
2025-10-01 11:41:41 -0600

    FermiPicoBagel

---
+++ fermiPicoBagel.cpp
@@ -0,0 +1,11 @@
+/*
+Fermi Pico Bagel - 10.1.25
+
+This program is a logic puzzle that generates a random 3-digit number 
+and asks the user to guess the correct answer using the hints provided 
+
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>S
